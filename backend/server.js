ef02e1550441f214203b4f8dfd753dd73a769867
naziyaShaikh <naziyahm1234567@gmail.com>
2025-05-07 21:55:27 +0530

    added mongose atlast connection for mongose error

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoutes');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todo-app', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
