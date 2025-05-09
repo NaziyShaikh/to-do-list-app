f8656fd455441011eacabff5d9e0540d31b064b5
naziyaShaikh <naziyahm1234567@gmail.com>
2025-05-09 21:19:43 +0530

    reslove the cors error in backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/database');
 const errorHandler = require('./middleware/errorHandler');
 
@@ -8,6 +9,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
